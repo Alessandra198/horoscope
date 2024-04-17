0aec9881d4c801b33727eed77d6b86f4c82e1d07
Alessandra198 <rinaldoalessandra@gmail.com>
2024-04-17 21:39:31 +0200

    Updated Options.js

--- src/Options.js
+++ src/Options.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function Options() {
-    return (
-      <div className="Options">
-        <select>
-          <option value="">Choose your Sign</option>
-          <option value="aries">Aries♈</option>
-          <option value="taurus">Taurus♉</option>
-          <option value="gemini">Gemini♊</option>
-          <option value="cancer">Cancer♋</option>
-          <option value="leo">Leo♌</option>
-          <option value="virgo">Virgo♍</option>
-          <option value="libra">Libra♎</option>
-          <option value="scorpio">Scorpio♏</option>
-          <option value="sagittarius">Sagittarius♐</option>
-          <option value="capricorn">Capricorn♑</option>
-          <option value="aqarius">Aquarius♒</option>
-          <option value="pisces">Pisces♓</option>
-        </select>
-      </div>
-    );
+  let [sign, setSign] = useState(null);
+  let apiUrl = `https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily?sign=${sign}&day=TODAY`;
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
+  function search() {
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleSign(event) {
+    setSign(event.target.value);
+  }
+  return (
+    <div className="Options">
+      <select onChange={handleSign}>
+        <option value="">Choose your Sign</option>
+        <option value="aries">Aries♈</option>
+        <option value="taurus">Taurus♉</option>
+        <option value="gemini">Gemini♊</option>
+        <option value="cancer">Cancer♋</option>
+        <option value="leo">Leo♌</option>
+        <option value="virgo">Virgo♍</option>
+        <option value="libra">Libra♎</option>
+        <option value="scorpio">Scorpio♏</option>
+        <option value="sagittarius">Sagittarius♐</option>
+        <option value="capricorn">Capricorn♑</option>
+        <option value="aqarius">Aquarius♒</option>
+        <option value="pisces">Pisces♓</option>
+      </select>
+      <button onClick={search}>Search</button>
+    </div>
+  );
 }
